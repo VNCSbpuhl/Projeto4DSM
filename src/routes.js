2a56764bb2573f774ab5b0649ed7018c5e79d45c
Vinicius <vinicius.r.o@hotmail.com>
2025-02-18 20:09:03 -0300

    Primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Main from "./pages/main";
+// import Login from "./pages/login";
+// import User from "./pages/user";
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return(
+       <Stack.Navigator>
+              {/* <Stack.Screen name="Login" component={Login} /> */}
+              <Stack.Screen name="Página Inicial" component={Main} />
+              {/* <Stack.Screen name="User" component={User} />   */}
+       </Stack.Navigator> 
+    )
+}
